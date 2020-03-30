f6d158a150372c2113d19b6fbd56aab7f696be27
Gabriel <cirstea6@gmail.com>
2020-03-30 23:26:41 +0300

    AFD

---
+++ AFD/REDME.md
@@ -0,0 +1,27 @@
+# Emulare AFD
+## Automat Finit Determinist
+
+Programul simuleaza un AFD, citi dintr-un fisier.
+__Formatul fisierului__:
+* Pe prima linie este indicele ultimei stari.
+* Urmeaza un sir de 0 si 1, reprezentat starile finale
+  * 1 starea este finala, 0 - nu este
+* Alfabetul: sir de caractere
+* n - Numarul de tranzitii
+* Urmeaza n tranzitii de forma:
+  * caracter sursa destinatie
+  * intre sursa si destinatie se poate face tranzitia cu caracterul citit
+* m - numarul de cuvinte care vor fi testate
+* Se citest cele m linii care vor fi testate de automat.
+
+## Algoritm
+Tranzitiile au fost retinulte intr-o matrice:
+* pe linii se afa indicii stariilor sursa
+* pe coloane sunt caracterele cu care se face tranzitia
+* la intersectia liniei cu coloana se afa starea in care se face tranzitia
+<br><br>
+Se pleaca din starea 0, se face parcurgerea din stare in stare cu literele cuvantului.
+Pentru fiecare litera a cuvantului se verifica daca estista:
+__Matrice[stareCurenta][litera]__;  
+In caz afirmativ, aceasta devine starea curenta.  
+In caz contrar nu avem tranzitie, deci cuvantul nu este acceptat.
---
+++ AFD/main.cpp
@@ -0,0 +1,123 @@
+#include <iostream>
+#include <vector>
+#include <list>
+#include <fstream>
+using namespace std;
+#define EMP_BUF(fisier) {\
+        char c = fisier.peek();\
+        while(c == '\n' || c == '\r')\
+        {\
+            fisier.get();\
+            c = fisier.peek();\
+        }\
+    }       //.get(), pt golirea buff-erului, in linux nu mai e \r dar prezenta lui din windows persista
+// nr de stari
+//lista de stari finale 1 - finala, 0 - nu
+//nr de adiacente
+//lista de adiacente: a 0 1 de la 0 la 1 cu 'a'
+//nr de cuvinte
+//cuvinte
+///chestiile citite pt automat
+int nrStari, stariFinale[20];
+//matrice de adiacenta
+int matrice[40][40] = {{-1}};
+int dimMatrice;
+string alfabet;
+int nrCuv;
+
+void afisare_matrice(int matrice[40][40], int dimMatrice)
+{///afisare matrice de tranzitii
+    cout<<"   ";
+    int size = alfabet.length();
+    for(int i = 0;i<size;i++)
+    {
+        cout<<alfabet[i]<<"  ";
+    }
+    cout<<endl;
+    for(int i = 0;i<dimMatrice;i++)
+    {
+        cout<<i<<": ";
+        for(int j = 0; j<size;j++)
+        {
+            if(matrice[i][j])
+            {
+                cout<<"'"<<matrice[i][j]<<"' ";
+            }
+            else
+                cout<<"' ' ";
+        }
+        cout<<endl;
+    }
+}
+
+void citire_imput(ifstream& fisier)
+{///citirea din fisier
+    fisier>>nrStari;
+    cout<<"nrStari = "<<nrStari<<endl;
+    fisier.get();
+    for(int i=0; i<=nrStari;i++)
+        fisier>>stariFinale[i];
+    EMP_BUF(fisier);
+    getline(fisier,alfabet);
+    cout<<"alfabet:"<<alfabet<<endl;
+    fisier>>dimMatrice;
+    for(int i=0;i<=nrStari;i++)
+    {
+        cout<<"Stare finala:"<<stariFinale[i]<<endl;
+    }
+    cout<<"dim:"<<dimMatrice<<endl;
+    for(int i = 0;i<dimMatrice;i++){
+        char litera;
+        int sursa,dest;
+        fisier>>litera;
+        fisier>>sursa;
+        fisier>>dest;
+        matrice[sursa][alfabet.find(litera,0)] = dest;
+    }
+    //pt afisare
+    dimMatrice = nrStari+1;
+    afisare_matrice(matrice,dimMatrice);
+}
+
+int delta(int stare, char c)
+{///functia delta
+    if(matrice[stare][alfabet.find(c,0)])
+        return matrice[stare][alfabet.find(c,0)];
+    return -1; ///eroare
+}
+
+int AFD(string cuvant)
+{
+    unsigned int i = 0;
+    int stareCurenta = 0;
+    while(i<cuvant.length())
+    {
+        stareCurenta = delta(stareCurenta, cuvant[i]);
+        if(stareCurenta < 0)
+            break;
+        i++;
+    }
+    if(stariFinale[stareCurenta])
+        return 1;
+    return 0;
+}
+
+int main()
+{
+    cout << "Hello world!" << endl;
+    ifstream f("toAFD.in");
+    citire_imput(f);
+    int nr_cuvinte;
+    f>>nr_cuvinte;
+    cout<<"cuvinte:"<<nr_cuvinte<<endl;
+    EMP_BUF(f);
+    string cuvant;
+    for(int i=0;i<nr_cuvinte;i++)
+    {
+        getline(f,cuvant);
+        EMP_BUF(f);
+        cout<<cuvant<<": ";
+        cout<<AFD(cuvant)<<endl;
+    }
+    return 0;
+}
