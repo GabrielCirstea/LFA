7be4a83d13c347a731fa0fd60b4cbc6cc709efaf
Gabriel <cirstea6@gmail.com>
2020-04-06 13:51:50 +0300

    AFD minimal

---
+++ AFDminimal/README.md
@@ -0,0 +1,35 @@
+# AFD minimal
+Se cauta un AFD echivalent cu cel dat, dar care sa aiba mai putine noduri.  
+
+___
+Programul citeste un AFD dintr-un fisier cu accelas format ca celelalte programe ce lucreaza cu automate si calculeaza AFD-ul minima, daca este posibil si il scrie intr-un fisier.  
+Pentru a fi acceptat de program AFD-ul trebuie sa fie __complet__, adica 
+sa aiba tranzitii cu toate literele din alfabet din fiecare stare.
+
+## Idei principale
+* trebuie aflate care sunt starile echivalente.
+* doua sau mai multe stari sunt __echivalente__ daca plecand din ele cu un sir 
+de caractere din alfabetul nostru ajungem in:
+    * stari finale
+    * sau in stari nefinale
+* starile __echivalente__ se unesc intr-o singura stare.
+### Algoritm
+* prima data se calculeaza o matrice de echivalenta pentru a sti care stari 
+sunt __echivalente__:
+    * matricea este simetrica si are 0 pe diagonala principala
+    * daca la intersectia liniilor si coloanelor i si j este 0 atunci starile 
+    i si j sunt echivalente
+    * se completeaza prima data liniile cu starile finale
+    * apoi se parcurge matricea la intalnirea unei valori 0, verificam daca plecand din starile i si j, ajungem in stari neechivalente, in caz afirmativ pe pozitia i,j se pune 1.
+    * matricea se parcurge astefel cat timp se produ asemenea schimbari
+* pentru a fi mai usor de tinut socoteala am facut o renotare a starilor pentru noul AFD
+* renotarea este un vector, in care indicii sunt starile AFD-ului normal 
+si valorile sunt starile din AFD-ul minimal.
+    * pentru fiecare stare ne renotata din AFD-ul normal, se face renotarea si se cauta si stari echivalente cu aceasta.
+* renotarea ne permite sa construim mai usor matricea de adiacenta a AFD-ului minim
+* pt starile din AFD nomal, se face tranzitia pt fiecare litera:
+  * in matricea pt AFD minim se trece tranzitia intre renotare[stare] si renotare[dest]
+  * se face renotarea atat pe starea din care pleaca tranzitia cat si pe cea 
+  in care ajunge
+* in cazul in care AFD-ul nu mai poate fi minimizat programul se opreste si scrie in fisier "NU!"
+* dupa ce minimizarea se opreste in fisierul de iesire vor fi scrise informatiile urmant formatul folosit si pentru fisierul de intrare.
---
+++ AFDminimal/main.cpp
@@ -0,0 +1,260 @@
+#include <iostream>
+#include <vector>
+#include <fstream>
+#include <list>
+#include <vector>
+using namespace std;
+
+#define VEC_SIZE 20     ///pt alocarea statica
+#define forit(it,lista) for(list<int>::iterator it=lista.begin();it!=lista.end();it++)
+int nrStari;
+int stariFinale[VEC_SIZE];
+string alfabet;
+int matrice[VEC_SIZE][VEC_SIZE];
+//-----
+//pentru AFD-ul minimal
+int stariMIN, finaleMIN[VEC_SIZE];
+int matriceMIN[VEC_SIZE][VEC_SIZE];
+void afisare()
+{///afisarea datelor cititea din fisier
+    cout<<"NrStari: "<<nrStari<<endl;
+    cout<<"Alfabet: "<<alfabet<<endl;
+    cout<<"Stari finale: ";
+    for(int i=0;i<=nrStari;i++)
+        cout<<stariFinale[i]<<" ";
+    cout<<endl;
+}
+void afisare_matrice(int matrice[20][20],int dimMatrice, int col)      ///afisarea pt AFN
+{
+    cout<<"   ";
+    for(unsigned int i = 0;i<alfabet.length();i++)
+        cout<<alfabet[i]<<" ";
+    cout<<endl;
+    for(int i = 0;i<dimMatrice;i++)
+    {
+        cout<<i<<":";
+        for(int j = 0;j<col;j++)
+        {
+            cout<<matrice[i][j]<<" ";
+        }
+        cout<<endl;
+    }
+}
+void initMatrice(int matrice[VEC_SIZE][VEC_SIZE],int n)
+{
+  for(int i=0;i<n;i++)
+    {
+      for(int j=0;j<n;j++)
+        matrice[i][j] = -1;
+    }
+}
+void citire_fisier(ifstream &f)
+{
+    f>>nrStari;
+    f.get();
+    initMatrice(matrice,nrStari+1);
+    getline(f,alfabet);
+    //citire stari finale
+    for(int i=0;i<=nrStari;i++)
+    {
+        f>>stariFinale[i];
+    }
+    int nrTranzitii;
+    f>>nrTranzitii;
+    for(int i=0;i<nrTranzitii;i++)
+    {
+        char litera;
+        int sursa,dest;
+        f>>litera;
+        f>>sursa;
+        f>>dest;
+        matrice[sursa][alfabet.find(litera,0)] = dest;
+    }
+    ///pt afisare
+    afisare();  ///afisarea datelor citite din fisier
+    afisare_matrice(matrice, nrStari+1,alfabet.size());
+}
+int delta(int stare, char c)
+{
+    return matrice[stare][alfabet.find(c,0)];
+}
+int verificareStare(int stare, int X[VEC_SIZE][VEC_SIZE])
+{//ne zice daca starea este echivalenta cu alte stari
+  for(int i=0;i<nrStari;i++)
+    {
+      if( i!=stare && X[stare][i]==0)
+        return 1;
+    }
+  return 0;
+}
+void renotare(int X[VEC_SIZE][VEC_SIZE],int notare[VEC_SIZE])
+{
+  for(int i=0;i<nrStari+1;i++)
+    //initiem vectorul cu -1, adica valori goale
+    notare[i] = -1;
+  int indiceStare = 0;
+  for(int i=0;i<nrStari+1;i++)
+    {//pentru fiecare stare nenotata verificam daca are stari echivalente
+      if(notare[i]<0)
+        {
+          notare[i] = indiceStare;
+          for(int j=i+1;j<nrStari;j++)
+          {//trecem toate starile echivalente ca aceeasi stare in noul AFD
+            if(!X[i][j])
+              notare[j] = indiceStare;
+          }
+          indiceStare++; // trecem la starea urmatoare
+        }
+    }
+  stariMIN = indiceStare-1;   //Asta va fi noul nr de stari
+}
+void populareMIN(int notare[VEC_SIZE])
+{
+  for(int i=0;i<nrStari+1;i++)
+    {
+      for(int c = 0;alfabet[c];c++)
+        {
+          int src = notare[i];                // luam starea renotata pt AFD minimal
+          int dest = delta(i,alfabet[c]);   //destinatia normala
+          //nu luam tranzitiile cu alte stari care se unesc cu asta
+          //putem pastra tranzitiile cu starea inseasi x - x
+          //daca avem tranzitie cu starea insasi
+          //sau nu este tranzitie cu o stare cu care se uneste
+          if(dest == i || notare[dest] != src)
+            //trecem in matri starea renotata
+            matriceMIN[src][c] = notare[dest];
+        }
+    }
+}
+void setareFinale(int notare[VEC_SIZE])
+{//seteaza starile finale ale AFD-ului minimal
+  for(int i = 0;i<nrStari+1;i++)
+    {
+      if(stariFinale[i])
+        finaleMIN[notare[i]] = 1;
+    }
+}
+int minimizare()
+{
+    ///avem matricea si alfabetul
+    int X[VEC_SIZE][VEC_SIZE]={{0}};
+    for(int i=0;i<nrStari+1;i++)
+    {
+        for(int j=i;j<nrStari+1;j++)
+        {
+            if(stariFinale[i] != stariFinale[j])
+                X[i][j] = 1,X[j][i] =1;
+            else
+                X[i][j] = 0,X[j][i] = 0;
+        }
+    }
+    bool changed;
+    do
+    {
+        changed = false;
+        for(int i=0;i<nrStari+1;i++)
+        {
+            for(int j=i+1;j<nrStari+1;j++)
+            {
+                if(!X[i][j])
+                {//verificam daca din starile i si j ajunge in stari ne echiv
+                    for(int k=0;alfabet[k];k++)
+                    {
+                        if(X[delta(i,alfabet[k])][delta(j,alfabet[k])])
+                        {
+                            X[i][j] = 1;
+                            X[j][i] = 1;
+                            changed = true;
+                        }
+                    }
+                }
+            }// for
+        }// for
+    }while(changed);
+    cout<<"Matrice echivalenta:\n";
+    afisare_matrice(X,nrStari+1,nrStari+1);
+
+    // o sa renotam starile, cu noi indici
+    //inclusiv starile pe care le unim
+    //cand facem o tranzitie din starea x in y
+    //il cautam pe y in noua renotare a starilor
+    int notare[VEC_SIZE];
+    renotare(X,notare);
+    if(nrStari == stariMIN)
+      {
+        cout<<"Nu poate fi minimizat!\n";
+        return 0;
+      }
+    cout<<"Renotarile starilor:\n";
+    for(int i=0;i<nrStari+1;i++)
+      {
+        cout<<i<<": "<<notare[i]<<endl;
+      }
+    populareMIN(notare);
+    setareFinale(notare);
+
+    return 1; // poate fi minimizat
+}
+void scrieAFD(ostream &out)
+{
+  out<<stariMIN<<endl;
+  out<<alfabet<<endl;
+  for(int i=0;i<stariMIN;i++)
+    out<<finaleMIN[i]<<" "; // problema: lasa un spatiu la sfarsit
+  out<<finaleMIN[stariMIN];	//se pune ultimul elemente, apoi \n
+  out<<endl;
+  // alfabet.size() * nr_de_stari daca ar fi complet
+  // din asta scadem campurile == -1 din matrice
+  int nrTranzitii = alfabet.size() * (stariMIN+1);
+  for(int i=0;i<stariMIN+1;i++)
+    {
+      for(int j=0;alfabet[j];j++)
+        {
+          if(matriceMIN[i][j]<0)
+            nrTranzitii--;
+        }
+    }
+  out<<nrTranzitii<<endl;
+  for(int i=0;i<stariMIN+1;i++)
+    {
+      for(int j=0;alfabet[j];j++)
+        {
+          if(matriceMIN[i][j]>-1)
+            out<<alfabet[j]<<" "<<i<<" "<<matriceMIN[i][j]<<endl;
+        }
+    }
+}
+int AFDcomplet(int nrStari, int matrice[VEC_SIZE][VEC_SIZE],string alfabet)
+{//verifica daca AFD-ul este complet
+  for(int i = 0;i<nrStari+1;i++)
+    {
+      for(int j=0;j<alfabet[j];j++)
+        if(matrice[i][j]<0)
+          return 0;
+    }
+  return 1;
+}
+int main()
+{
+    cout << "Hello world!" << endl;
+    ifstream f("date.in");
+    ofstream F("iesire.out");
+    ostream& out = F;
+    citire_fisier(f);
+    if(!AFDcomplet(nrStari,matrice,alfabet))
+      {
+        out<<"Nu e complet!\n;";
+        return 0;
+      }
+    if(!minimizare())
+      {
+        out<<"NU!\n";
+        return 0;
+      }
+    cout<<"Matrice adiacenta AFD minimal\n";
+    afisare_matrice(matriceMIN,stariMIN+1,alfabet.size());
+    scrieAFD(out);
+    f.close();
+    F.close();
+    return 0;
+}
